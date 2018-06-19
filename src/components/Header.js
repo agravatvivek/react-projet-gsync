b6f3b183d61062d6a7b3e2ef0d4cf58606bc882e
aklmac2 <aklmac2@aklmac2s-MBP.fritz.box>
2018-06-19 22:44:46 +1200

    google link test

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,8 @@ const Header = (props) => (
   </div>
   <div className="header-menu">
     <div className="container-dark">
-      <NavLink to="/gmail" activeClassName="is-active" className="button-menu">Gmail</NavLink>
+      <!--<NavLink to="/gmail" activeClassName="is-active" className="button-menu">Gmail</NavLink>-->
+      <a href="https://www.thesitewizard.com/" target="_blank" className="button-menu">thesitewizard.com</a>
       <NavLink to="/calender" activeClassName="is-active" className="button-menu">Calender</NavLink>
       <NavLink to="/forms" activeClassName="is-active" className="button-menu">Forms</NavLink>
       <NavLink to="/drive" activeClassName="is-active" className="button-menu">Drive</NavLink>
