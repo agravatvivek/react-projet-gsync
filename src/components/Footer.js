0c57ac362109ee8c9c69ee91f298eb7d28e18de2
vivek <vivek.agravat177@gmail.com>
2018-07-17 20:14:54 +1200

    final disply

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,64 @@ const Footer = () => (
   <div>
     <div className="footer">
       <div className="container">
-        <h1 className="header__title">Footer</h1>
-        <h2 className="header__subtitle">Subtitle</h2>
+      <table className="table-footer">
+        <tr>
+          <td className="tableRow-footer">
+            <tr>
+              <h3 className="footer__title">
+                Gsync
+               </h3>
+            </tr>
+            <tr>
+              <h3 className="footer__subtitle">Gsync is Mini project dedicated to google services.This application integrates most of the applications that the client is using for a daily basis. Instead of switch from one application to another all the time, this web application could provide the frequent functions on one page. Application is developed by students of Otago Polytechnic
+              </h3>
+            </tr>
+          </td>
+          <td className="tableRow-footer__last">
+            <tr>
+              <h3 className="footer__title">
+                Project supervisor
+              </h3>
+            </tr>
+            <tr>
+              <h3 className="footer__title">
+                Dr.Foad Marzoughi
+              </h3>
+            </tr>
+            <tr/>
+            <tr>
+              <h3 className="footer__subtitle__developers__title">
+                Dedicated Developers
+              </h3>
+            </tr>
+            <tr>
+              <h3 className="footer__subtitle__developers">
+                Divyang Pansuriya
+              </h3>
+            </tr>
+            <tr>
+              <h3 className="footer__subtitle__developers">
+                Eugene Marikhin
+              </h3>
+            </tr>
+            <tr>
+              <h3 className="footer__subtitle__developers">
+                Gao Liang
+              </h3>
+            </tr>
+            <tr>
+              <h3 className="footer__subtitle__developers">
+                Sagar Patel
+              </h3>
+            </tr>
+            <tr>
+              <h3 className="footer__subtitle__developers">
+                Vivek Agravat
+              </h3>
+            </tr>
+          </td>  
+        </tr>
+      </table>
       </div>
     </div>
   </div>
